b91c453c954c8c35c096715a8894d4f396161756
owen.le.goldenowl <owen.le.goldenowl@gmail.com>
2019-09-17 11:59:31 +0700

    implement profile pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update destroy]
+
   def new
     @user = User.new
   end
@@ -9,16 +11,35 @@ class UsersController < ApplicationController
       flash[:notice] = 'You have signed up successfully'
       redirect_to projects_path
     else
-      render :new
+      flash[:alert] = 'Fail to sign up'
+      render 'new'
     end
   end
 
-  def show
+  def show;
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = 'Profile has been updated'
+      redirect_to @user
+    else
+      flash[:alert] = 'Fail to update profile'
+      render 'edit'
+    end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
---
+++ spec/factories/user_factory.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :user do
+    name { 'example username' }
+    email { 'email@example.com' }
+    password { 'password' }
+  end
+end
