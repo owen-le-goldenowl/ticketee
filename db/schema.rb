c1e6236dd66202531071fed7dba0b96ea0b51ede
owen.le.goldenowl <owen.le.goldenowl@gmail.com>
2019-09-18 17:28:43 +0700

    add admiin flag to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_18_095216) do
+ActiveRecord::Schema.define(version: 2019_09_18_100543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2019_09_18_095216) do
     t.string "salt"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
   end
 
   add_foreign_key "tickets", "projects"
