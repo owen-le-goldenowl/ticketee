054485fdcc8cb10b9ce49efeb9cb7b3dff3210be
owen.le.goldenowl <owen.le.goldenowl@gmail.com>
2019-09-18 14:43:38 +0700

    iplement sign up

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = 'You have sign up successfully'
+      redirect_to projects_path
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit[:name, :password, :password_confirmation, :email]
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/create'
+  get 'users/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'projects#index'
   resources :projects do
     resources :tickets
   end
+  resources :users
   match '*path', to: 'errors#not_found', via: :all
 end
