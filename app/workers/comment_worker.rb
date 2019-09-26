e633ace7234ba2c63c8a73a1a462b855d045ab21
owen.le.goldenowl <owen.le.goldenowl@gmail.com>
2019-09-26 21:49:44 +0700

    update database.yml not use localhost

--- app/workers/comment_worker.rb
+++ app/workers/comment_worker.rb
@@ -1,6 +1,5 @@
 class CommentWorker
   include Sidekiq::Worker
-
   def perform(comment_id, user_id, comment_creator_id)
     NotifierMailer.comment_updated(comment_id, user_id, comment_creator_id).deliver
   end
